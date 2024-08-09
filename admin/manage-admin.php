507fad7221a91a3113e78008b30ea3105f6906fc
binamra-linux <53526396+binamra-linux@users.noreply.github.com>
2024-08-09 11:14:03 +0545

    Added buttons in Admin Page

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -4,7 +4,38 @@
 <div class="main-content">
 <div class="wrapper">
     <h1>Manage Admin</h1>
+    <br><br><br>
+    <!-- Add admin button -->
+    <a href="" class="btn-primary">Add Admin</a>
+    <br><br><br>
+    <table class="table-full">
+        <tr>
+            <th>ID</th>
+            <th>Full Name</th>
+            <th>Username</th>
+            <th>Action</th>
+        </tr>
 
+        <tr>
+            <td>1. </td>
+            <td>Binamra Pandey</td>
+            <td>Binamra</td>
+            <td>
+                Update Admin
+                Delete Admin
+            </td>
+        </tr>
+
+        <tr>
+            <td>2. </td>
+            <td>Pratik Rajthala</td>
+            <td>pratik</td>
+            <td>
+                <a href="#" class="btn-secondary">Update Admin</a>
+                <a href="#" class="btn-danger">Delete Admin</a>
+            </td>
+        </tr>
+    </table>
 
     </div>
 </div>
